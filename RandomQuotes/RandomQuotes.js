4a8cffcaa9c5b768b9434116ae2930211adfb104
Neal Homan <homanne@oregonstate.edu>
2024-09-05 15:44:45 -0700

    again 2

--- RandomQuotes/RandomQuotes.js
+++ RandomQuotes/RandomQuotes.js
@@ -13,11 +13,13 @@ const store = createStore(
 );*/
 import { createRoot } from 'react-dom/client';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+function NavigationBar() {
+    // TODO: Actually implement a navigation bar
+    return <h1>Hello from React!</h1>;
+  }
+  
+  const domNode = document.getElementById('quote-box');
+  const root = createRoot(domNode);
+  root.render(<NavigationBar />);
 
 
