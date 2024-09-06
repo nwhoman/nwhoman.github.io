cbb044eb26bfee67dd756a9da36a9311f033c9c4
Neal Homan <homanne@oregonstate.edu>
2024-09-06 13:58:52 -0700

    add quotes

--- RandomQuotes/RandomQuotes.js
+++ RandomQuotes/RandomQuotes.js
@@ -1,25 +1,71 @@
 'use strict';
 const getQuote = document.getElementById("new-quote");
+
+const quotes = [
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney"
+    },
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela"
+    },
+    {
+        quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+        author: "Steve Jobs"
+    },
+    {
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+        author: "Oprah Winfrey"
+    },
+    {
+        quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        author: "James Cameron"
+    },
+    {
+        quote: "You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.",
+        author: "John Lennon"
+    },
+]
 const e = React.createElement;
 
-function newQuote(){
-
+function pickQuote(){
+    const randNum = Math.floor(Math.random * 6);
+    return quotes[randNum];
 }
 
 class DisplayText extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    this.state = { quote: '',
+        author: ''
+     };
+     this.getQuote = this.getQuote.bind(this);
+
   }
+  getQuote(event){
+    const newQuote = pickQuote();
+  
+    this.setState({
+      quote: newQuote.quote,
+        author: newQuote.author
+      
+    });
+  
+}
 
   render() {
     return <div  id="quote-box">Neal Homan
                 <div id="text">
-                    Quote Text
+                    {this.state.quote}
                 </div>
-                <button id="search-button" >New Quote</button>
+                <button id="search-button" onClick={this.getQuote}>New Quote</button>
                 <div id="author">
-                    by Author
+                    - {this.state.author}
                 </div>
             </div>;
   }
