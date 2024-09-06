545c64370381a2046291849d2b1c07d0b6ae9eb8
Neal Homan <homanne@oregonstate.edu>
2024-09-06 15:55:31 -0700

    buttons css

--- RandomQuotes/RandomQuotes.js
+++ RandomQuotes/RandomQuotes.js
@@ -69,8 +69,8 @@ class DisplayText extends React.Component {
                     - {this.state.author}
                 </div>
                 <span>
-                    <a id="tweet-quote" href="https://www.twitter.com/intent/tweet"><button >Tweet</button></a>
-                    <button id="new-quote" onClick={this.getQuote}>New Quote</button>
+                    <a id="tweet-quote" href="https://www.twitter.com/intent/tweet" class="button" ><button >Tweet</button></a>
+                    <button class="button" id="new-quote" onClick={this.getQuote}>New Quote</button>
                 </span>
             </div>;
   }
