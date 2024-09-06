02e5ca75b90a657028305016c9d4d097597d530d
Neal Homan <homanne@oregonstate.edu>
2024-09-06 10:25:43 -0700

    new try

--- RandomQuotes/RandomQuotes.js
+++ RandomQuotes/RandomQuotes.js
@@ -1,6 +1,6 @@
-import React from 'react'
+/*import React from 'react'
 import ReactDOM from 'react-dom'
-/*import { Provider, connect } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
